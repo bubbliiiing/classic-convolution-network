815868c6d76a984b4c84e84aabc69a909bd7ba0c
JiaQi Xu <47347516+bubbliiiing@users.noreply.github.com>
2019-12-12 09:43:24 +0800

    Add files via upload

---
+++ VGG16.py
@@ -0,0 +1,84 @@
+import tensorflow as tf
+from tensorflow import keras
+from keras import Model,Sequential
+from keras.layers import Flatten, Dense, Conv2D, GlobalAveragePooling2D
+from keras.layers import Input, MaxPooling2D, GlobalMaxPooling2D
+from keras.utils.data_utils import get_file
+from keras.preprocessing import image
+from keras.applications.imagenet_utils import decode_predictions
+import numpy as np
+WEIGHTS_PATH = 'https://github.com/fchollet/deep-learning-models/releases/download/v0.1/vgg16_weights_tf_dim_ordering_tf_kernels.h5'
+
+
+def VGG16(num_classes):
+    # 224，224，3
+    image_input = Input(shape = (224,224,3))
+    # 第一个卷积部分
+    # 112，112，64
+    x = Conv2D(64,(3,3),activation = 'relu',padding = 'same',name = 'block1_conv1')(image_input)
+    x = Conv2D(64,(3,3),activation = 'relu',padding = 'same', name = 'block1_conv2')(x)
+    x = MaxPooling2D((2,2), strides = (2,2), name = 'block1_pool')(x)
+
+    # 第二个卷积部分
+    # 56,56,128
+    x = Conv2D(128,(3,3),activation = 'relu',padding = 'same',name = 'block2_conv1')(x)
+    x = Conv2D(128,(3,3),activation = 'relu',padding = 'same',name = 'block2_conv2')(x)
+    x = MaxPooling2D((2,2),strides = (2,2),name = 'block2_pool')(x)
+
+    # 第三个卷积部分
+    # 28,28,256
+    x = Conv2D(256,(3,3),activation = 'relu',padding = 'same',name = 'block3_conv1')(x)
+    x = Conv2D(256,(3,3),activation = 'relu',padding = 'same',name = 'block3_conv2')(x)
+    x = Conv2D(256,(3,3),activation = 'relu',padding = 'same',name = 'block3_conv3')(x)
+    x = MaxPooling2D((2,2),strides = (2,2),name = 'block3_pool')(x)
+
+    # 第四个卷积部分
+    # 14,14,512
+    x = Conv2D(512,(3,3),activation = 'relu',padding = 'same', name = 'block4_conv1')(x)
+    x = Conv2D(512,(3,3),activation = 'relu',padding = 'same', name = 'block4_conv2')(x)
+    x = Conv2D(512,(3,3),activation = 'relu',padding = 'same', name = 'block4_conv3')(x)
+    x = MaxPooling2D((2,2),strides = (2,2),name = 'block4_pool')(x)
+
+    # 第五个卷积部分
+    # 7,7,512
+    x = Conv2D(512,(3,3),activation = 'relu',padding = 'same', name = 'block5_conv1')(x)
+    x = Conv2D(512,(3,3),activation = 'relu',padding = 'same', name = 'block5_conv2')(x)
+    x = Conv2D(512,(3,3),activation = 'relu',padding = 'same', name = 'block5_conv3')(x)    
+    x = MaxPooling2D((2,2),strides = (2,2),name = 'block5_pool')(x)
+    # 提取特征
+
+    # 分类部分
+    # 7x7x512
+    # 25088
+    x = Flatten(name = 'flatten')(x)
+    # 256
+    x = Dense(256,activation = 'relu',name = 'fullc1')(x)
+    # 256
+    x = Dense(256,activation = 'relu',name = 'fullc2')(x)
+    # 2
+    x = Dense(num_classes,activation = 'softmax',name = 'fullc3')(x)
+    model = Model(image_input,x,name = 'vgg16')
+
+    return model
+
+def preprocess_input(x):
+    x /= 255.
+    x -= 0.5
+    x *= 2.
+    return x
+
+if __name__ == '__main__':
+    model = VGG16(1000)
+    weights_path = get_file('vgg16_weights_tf_dim_ordering_tf_kernels.h5',
+                                    WEIGHTS_PATH,
+                                    cache_subdir='models')
+    model.load_weights(weights_path)
+    img_path = 'elephant.jpg'
+    img = image.load_img(img_path, target_size=(224, 224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    print('Input image shape:', x.shape)
+
+    preds = model.predict(x)
+    print('Predicted:', decode_predictions(preds))
